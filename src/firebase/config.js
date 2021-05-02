ab7938da6dbbd0e34bde501eabd98ea748e89416
shinnosuke-nakamura <6jo.oneroom@gmail.com>
2021-05-02 16:03:14 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyBarEh26zmpBnh2_oV0hClecBcYqiHLlWg",
+    authDomain: "tabelog-match.firebaseapp.com",
+    projectId: "tabelog-match",
+    storageBucket: "tabelog-match.appspot.com",
+    messagingSenderId: "981945044635",
+    appId: "1:981945044635:web:5ea82ada70fec28ebac03e",
+    measurementId: "G-TFHV5QK3KV"
+};
